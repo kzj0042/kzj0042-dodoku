781e030b1852c285c18589938cc5c6890d8be8f2
kzj0042 <kzj0042@auburn.edu>
2021-09-19 10:24:20 -0500

    Green Light |

--- dodoku/sandbox/__init__.py
+++ dodoku/sandbox/__init__.py
@@ -1,4 +1,6 @@
 grid=[0,1,2,3,4]
 hash=1234
-result = {'status':'ok', 'grid:':grid,'hash':hash}
-print(result)   
+result = {'op':'create&level=2'}
+results = result['op']
+split = results.split('=')
+print(split[1])   
--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -10,7 +10,7 @@ class CreateTest(TestCase):
 
         def test_002_ShouldCreateLevel1Grid(self):
             expectedResult = {"grid":[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,0,-6,0,0,-3,0,0,0,0,-4,0,-5,-7,0,0,0,0,0,0,-6,-2,0,0,-7,0,-9,0,-5,0,-4,0,0,0,-6,0], 'status':'ok'}
-            parms={'op':'create'}
+            parms={'op':'create&level=1'}
             actualResult = create._create(parms)
             self.assertDictEqual(expectedResult, actualResult)
             
