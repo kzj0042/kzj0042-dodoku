aa0680bd9f87b88b002ae7032526c98d008121eb
kzj0042 <kzj0042@auburn.edu>
2021-09-19 10:26:05 -0500

    Green Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -2,12 +2,6 @@ from unittest import TestCase
 import dodoku.create as create 
 
 class CreateTest(TestCase):
-        def test_001_ShouldReturnStatusOk(self):
-            expectedResult = {"status":"ok"}
-            parms = {'op':'create'}
-            actualResult = create._create(parms)
-            self.assertDictContainsSubset(expectedResult, actualResult)
-
         def test_002_ShouldCreateLevel1Grid(self):
             expectedResult = {"grid":[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,0,-6,0,0,-3,0,0,0,0,-4,0,-5,-7,0,0,0,0,0,0,-6,-2,0,0,-7,0,-9,0,-5,0,-4,0,0,0,-6,0], 'status':'ok'}
             parms={'op':'create&level=1'}
