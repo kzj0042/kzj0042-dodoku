09ff3f557a50a26ca378b52584009f25da230c6a
kzj0042 <kzj0042@auburn.edu>
2021-09-19 10:04:57 -0500

    Red Light |

--- dodoku/sandbox/__init__.py
+++ dodoku/sandbox/__init__.py
@@ -1,1 +1,4 @@
-   
+grid=[0,1,2,3,4]
+hash=1234
+result = {'status':'ok', 'grid:':grid,'hash':hash}
+print(result)   
--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -2,4 +2,8 @@ from unittest import TestCase
 import dodoku.create as create 
 
 class CreateTest(TestCase):
-        pass
+        def test_001_ShouldReturnStatusOk(self):
+            expectedResult = {"status":"ok"}
+            parms = {'op':'create'}
+            actualResult = create._create(parms)
+            self.assertDictEqual(expectedResult, actualResult)
