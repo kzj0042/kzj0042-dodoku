efc386c1d1139c0f8ee0dbe34e283ff9bae75077
kzj0042 <kzj0042@auburn.edu>
2021-09-19 10:18:36 -0500

    Red Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -13,3 +13,9 @@ class CreateTest(TestCase):
             parms={'op':'create'}
             actualResult = create._create(parms)
             self.assertDictEqual(expectedResult, actualResult)
+            
+        def test_003_ShouldCreateLevel2Grid(self):
+            expectedResult = {"grid":[0,-6,0,0,0,0,0,-5,-9,-9,-3,0,-4,-8,0,0,0,0,0,0,0,0,0,-7,-3,0,0,0,-5,0,0,-1,0,0,-4,-6,0,0,0,0,0,-6,0,-9,0,0,-8,-1,-2,0,0,0,0,0,0,0,0,0,-7,0,0,0,0,-8,0,-4,0,0,-1,0,0,0,0,-9,0,0,0,0,0,0,0,0,0,0,0,-7,0,-5,-8,0,0,0,-2,0,0,0,-6,-1,0], 'status':'ok'}
+            parms={'op':'create&level=2'}
+            actualResult = create._create(parms)
+            self.assertDictEqual(expectedResult, actualResult)
