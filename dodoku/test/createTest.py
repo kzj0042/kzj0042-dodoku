c65e8abefcefa998fac452c20f4e0acd2ba96abe
kzj0042 <kzj0042@auburn.edu>
2021-09-19 12:26:14 -0500

    Red Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -10,7 +10,7 @@ class CreateTest(TestCase):
             self.assertDictEqual(expectedResult, actualResult)
             
         def test_003_ShouldCreateLevel2Grid(self):
-            expectedResult = {"grid":[0,-6,0,0,0,0,0,-5,-9,-9,-3,0,-4,-8,0,0,0,0,0,0,0,0,0,-7,-3,0,0,0,-5,0,0,-1,0,0,-4,-6,0,0,0,0,0,-6,0,-9,0,0,-8,-1,-2,0,0,0,0,0,0,0,0,0,-7,0,0,0,0,-8,0,-4,0,0,-1,0,0,0,0,-9,0,0,0,0,0,0,0,0,0,0,0,-7,0,-5,-8,0,0,0,-2,0,0,0,-6,-1,0], 'status':'ok'}
+            expectedResult = {"grid":[0,-6,0,0,0,0,0,-5,-9,-9,-3,0,-4,-8,0,0,0,0,0,0,0,0,0,-7,-3,0,0,0,-5,0,0,-1,0,0,-4,-6,0,0,0,0,0,-6,0,-9,0,0,-8,-1,-2,0,0,0,0,0,0,0,0,0,-7,0,0,0,0,-8,0,-4,0,0,-1,0,0,0,0,-9,0,0,0,0,0,0,0,0,0,0,0,-7,0,-5,-8,0,0,0,-2,0,0,0,-6,-1,0], 'status':'ok', 'integrity':'35ac31f7'}
             parms={'op':'create', 'level':'2'}
             actualResult = create._create(parms)
             self.assertDictEqual(expectedResult, actualResult)
