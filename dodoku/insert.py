7109df9e56c0602dd006a84424b179cb18cd30f3
kzj0042 <kzj0042@auburn.edu>
2021-10-06 21:23:45 -0500

    Green Light |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -42,7 +42,7 @@ def _insert(parms):
     else:
         try:
             value = int(parms['value'])
-            if value < 0 or value>9:
+            if value < 1 or value>9:
                 result = {'status':'error: invalid value'}
                 return result
         except ValueError:
