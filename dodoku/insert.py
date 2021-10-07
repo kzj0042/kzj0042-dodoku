12234332792ac4e079f1b9b93430b9a1355eb580
kzj0042 <kzj0042@auburn.edu>
2021-10-06 19:12:45 -0500

    Green Light |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -129,6 +129,7 @@ def _insert(parms):
         if value in map(abs,rows[rowNum-1]) and value>0:
             status = 'warning'
         elif rows[rowNum-1][colNum-1]<0:
+            print('here')
             result = {'status':'error: attempt to change fixed hint'}
             return result
         rows[rowNum-1][colNum-1] = value
