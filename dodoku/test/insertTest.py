2b2bf62921e7781541411b139843d4a0dc2c314a
kzj0042 <kzj0042@auburn.edu>
2021-10-06 19:06:53 -0500

    Red Light |

--- dodoku/test/insertTest.py
+++ dodoku/test/insertTest.py
@@ -219,7 +219,7 @@ class InsertTest(TestCase):
         def test_102_ShouldNotInsertInvalidValue(self):
             grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,
                     -2,0,0,-6,0,0,0,0,3,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]                                
-            parms = {'op':'insert', 'cell':'r10c1', 'value':'a', 'grid':grid, 'integrity':create.calculateHash(grid)}
+            parms = {'op':'insert', 'cell':'r7c9', 'value':'a', 'grid':grid, 'integrity':create.calculateHash(grid)}
             actualResult = insert._insert(parms)
             expectedResult = 'error: invalid value'
             actualStatus = actualResult['status']
