da7997aa03994426a0aef986c599eb4cc99679d0
kzj0042 <kzj0042@auburn.edu>
2021-10-07 11:58:41 -0500

    Refactor |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import dodoku.create as create 
+import dodoku.utilities as utilities
 
 class CreateTest(TestCase):
         #happy path tests
@@ -76,17 +77,25 @@ class CreateTest(TestCase):
                     
         def test_006_shouldCreateSha256HashLevel1(self):
             expectedResult = '5a3f0c31993d46bcb2ab5f3e8318e734231ee8bdb26cba545fadd7b1732888cd'
-            actualResult = create.calculateHash('1')
+            actualResult = utilities.getEightCharactersOfHash(utilities.calculateHash('1'))
             self.assertIn(actualResult, expectedResult)
             
         def test_007_shouldCreateSha256HashLevel2(self):
             expectedResult = '6fcd71ef7722e7573d2f607a35cfa48f72b03c4cea135ac31f7ef73a58e50a8a'
-            actualResult = create.calculateHash('2')
+            grid = [0,-6,0,0,0,0,0,-5,-9,-9,-3,0,-4,-8,0,0,0,0,0,0,0,0,0,-7,-3,0,0,0,-5,0,0,-1,0,0,-4,-6,0,0,0,0,0,-
+6,0,-9,0,0,-8,-1,-2,0,0,0,0,0,0,0,0,0,-7,0,0,0,0,0,0,0,0,-5,0,-8,0,-4,0,0,-1,0,0,0,-7,0,0,-6,0,-2,0,-
+9,0,0,0,0,0,0,0,0,-5,0,0,0,0,0,0,0,0,0,-9,-5,-3,0,0,-7,0,-4,0,0,0,0,0,-5,-8,0,0,-1,0,0,-9,0,0,0,-2,-
+1,0,0,0,0,0,0,0,0,0,-9,-8,0,-6,-1,-6,-1,0,0,0,0,0,-7,0]
+            actualResult = utilities.getEightCharactersOfHash(utilities.calculateHash(grid))
             self.assertIn(actualResult, expectedResult)
 
         def test_008_shouldCreateSha256HashLevel3(self):
             expectedResult = 'eb572835ffe2015c731057f94d46fa77430ad6fd332abb0d7dd39d5f2ccadea9'
-            actualResult = create.calculateHash('3')
+            grid = [0,0,0,0,-6,0,0,0,0,0,0,0,-4,0,-9,0,0,0,0,0,-9,-7,0,-5,-1,0,0,0,-5,-2,0,-7,0,-8,-9,0,-9,0,0,-
+5,0,-2,0,0,-4,0,-8,-3,0,-4,0,-7,-2,0,0,0,-1,-2,0,-8,0,0,0,0,-3,0,0,0,0,0,0,0,-6,0,-4,0,0,0,-8,0,-
+7,0,0,0,0,0,0,0,-5,0,0,0,0,-1,0,-6,-3,0,0,0,-9,-8,0,-5,0,-1,-2,0,-2,0,0,-7,0,-1,0,0,-3,0,-4,-3,0,-8,0,-
+6,-5,0,0,0,-7,-3,0,-5,-9,0,0,0,0,0,-4,0,-2,0,0,0,0,0,0,0,-6,0,0,0,0]
+            actualResult = utilities.getEightCharactersOfHash(utilities.calculateHash(grid))
             self.assertIn(actualResult, expectedResult)
             
         def test_009_ShouldCreateLevelWithNoLevelParameter(self):
