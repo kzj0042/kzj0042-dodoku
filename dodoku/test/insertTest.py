c2fc25534cf651b44e4a2180e18f1f1b10e57029
kzj0042 <kzj0042@auburn.edu>
2021-10-07 14:28:11 -0500

    Red Light |

--- dodoku/test/insertTest.py
+++ dodoku/test/insertTest.py
@@ -277,6 +277,15 @@ class InsertTest(TestCase):
             actualResult = insert._insert(parms)
             expectedResult = 'error: invalid cell reference' 
             actualStatus = actualResult['status']
-            self.assertEqual(expectedResult, actualStatus)       
+            self.assertEqual(expectedResult, actualStatus)      
+            
+        def test_109_ShouldNotInsertValueNegativeColumn(self):
+            grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,
+                    -2,0,0,-6,0,0,0,0,3,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]     
+            parms = {'op':'insert', 'cell':'r5c-1', 'value':'3', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}      
+            actualResult = insert._insert(parms)
+            expectedResult = 'error: invalid cell reference' 
+            actualStatus = actualResult['status']
+            self.assertEqual(expectedResult, actualStatus)              
         
             
