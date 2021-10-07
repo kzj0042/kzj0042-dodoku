342a0d09524828ca123c37377cbccbb5955905be
kzj0042 <kzj0042@auburn.edu>
2021-10-06 21:22:26 -0500

    Red Light |

--- dodoku/test/insertTest.py
+++ dodoku/test/insertTest.py
@@ -261,5 +261,13 @@ class InsertTest(TestCase):
             actualStatus = actualResult['status']
             self.assertEqual(expectedResult, actualStatus)                      
             
+        def test_107_ShouldNotInsertValueTooSmall(self):
+            grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,
+                    -2,0,0,-6,0,0,0,0,3,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]                                
+            parms = {'op':'insert', 'cell':'r7c9', 'value':'0', 'grid':grid, 'integrity':create.getEightCharactersOfHash(create.calculateHash(grid))}
+            actualResult = insert._insert(parms) 
+            expectedResult = 'error: invalid value'
+            actualStatus = actualResult['status']
+            self.assertEqual(expectedResult, actualStatus)           
         
             
