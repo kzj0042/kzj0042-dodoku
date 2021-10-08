a4ef0ba306f547de3cb4bba1f4c04d16e8f757d9
kzj0042 <kzj0042@auburn.edu>
2021-10-08 13:11:33 -0500

    Green Light |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -9,7 +9,7 @@ def _insert(parms):
         result = {'status':'error: invalid grid'}
     elif 'integrity' not in parms:
         result = {'status':'error: integrity mismatch'}
-    elif not parms['cell'].lower().startswith('r') or (parms['cell'].lower()[2]!='c' and parms['cell'].lower()[3]!='c') or len(parms['cell']) < 4:
+    elif len(parms['cell']) < 4 or not parms['cell'].lower().startswith('r') or (parms['cell'].lower()[2]!='c' and parms['cell'].lower()[3]!='c'):
         result = {'status': 'error: missing cell reference'}
     else:
         grid = parms['grid']
