e199f32f6e4bc52152a2bb296f5665c7eb2831d5
kzj0042 <kzj0042@auburn.edu>
2021-10-22 12:27:10 -0500

    Green Light |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -69,15 +69,15 @@ def _insert(parms):
         colMajorOrder = changeMajorOrder.convertToColMajorOrder(grid)
         if colNum>6 and colNum<10 and value>0:
             if rowNum<=6:
-                colList = colMajorOrder[colNum-1][0:9]
+                colList = map(abs, colMajorOrder[colNum-1][0:9])
                 if str(value) in colList:
                     status = 'warning'
             elif rowNum>9:
-                colList = colMajorOrder[colNum-1][6:15]
+                colList = map(abs, colMajorOrder[colNum-1][6:15])
                 if str(value) in colList:
                     status = 'warning'
             else: 
-                if str(value) in colMajorOrder[colNum-1] and value > 0:
+                if str(value) in map(abs, colMajorOrder[colNum-1]) and value > 0:
                     status = 'warning' 
         
     
