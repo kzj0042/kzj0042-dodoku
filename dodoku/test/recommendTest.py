c57fcd32cec9b35d204108d32ffd9b8930ce75c5
kzj0042 <kzj0042@auburn.edu>
2021-11-15 20:41:17 -0600

    Red Light |

--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -171,5 +171,14 @@ class RecommendTest(TestCase):
             expectedResult = 'error: invalid cell reference' 
             actualStatus = actualResult['status']
             self.assertEqual(expectedResult, actualStatus)
+
+        def test_114_ShouldReturnErrorValueRowNotGiven(self):
+            grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,
+                    -2,0,0,-6,0,0,0,0,3,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]     
+            parms = {'op':'recommend', 'cell':'c3', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}      
+            actualResult = recommend._recommend(parms)
+            expectedResult = 'error: missing cell reference' 
+            actualStatus = actualResult['status']
+            self.assertEqual(expectedResult, actualStatus)  
             
         
