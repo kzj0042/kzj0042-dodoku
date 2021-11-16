f062ea71c27460f38866923757f23b6201054f3c
kzj0042 <kzj0042@auburn.edu>
2021-11-15 20:58:16 -0600

    Red Light |

--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -97,7 +97,7 @@ class RecommendTest(TestCase):
             actualStatus = {'status':actualResult['status']}
             actualRecommendation = {'recommendation':actualResult['recommendation']}
             self.assertDictEqual(expectedStatus, actualStatus)
-            self.assertDictEqual(expectedRecommendation, actualRecommendation)                               
+            self.assertDictEqual(expectedRecommendation, actualRecommendation)                                
         
         #sad path
         def test_100_ShouldReturnErrorOutOfBoundsCell(self):
@@ -266,4 +266,13 @@ class RecommendTest(TestCase):
             expectedResult = 'error: invalid cell reference'
             actualStatus = actualResult['status']
             self.assertEqual(expectedResult, actualStatus)
+            
+        def test_120_ShouldReturnErrorColumnBeforeRow(self):
+            grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,
+                    -2,0,0,-6,0,0,0,0,3,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]     
+            parms = {'op':'recommend', 'cell':'c5r5', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}      
+            actualResult = recommend._recommend(parms)
+            expectedResult = 'error: invalid cell reference' 
+            actualStatus = actualResult['status']
+            self.assertEqual(expectedResult, actualStatus)            
         
