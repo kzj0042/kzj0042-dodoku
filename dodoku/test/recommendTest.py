093a2db26009154ad95f5301b413464aea349485
kzj0042 <kzj0042@auburn.edu>
2021-11-15 20:39:38 -0600

    Red Light |

--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -154,4 +154,13 @@ class RecommendTest(TestCase):
             actualStatus = actualResult['status']
             self.assertEqual(expectedResult, actualStatus)
             
+        def test_112_ShouldReturnErrorValueBigRow(self):
+            grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,
+                    -2,0,0,-6,0,0,0,0,3,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]     
+            parms = {'op':'insert', 'cell':'r16c5', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}      
+            actualResult = recommend._recommend(parms)
+            expectedResult = 'error: invalid cell reference' 
+            actualStatus = actualResult['status']
+            self.assertEqual(expectedResult, actualStatus) 
+            
         
