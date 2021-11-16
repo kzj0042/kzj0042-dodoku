6b94ed8b3a1e451cbdc0d3d39d5feee624d6bd48
kzj0042 <kzj0042@auburn.edu>
2021-11-15 19:35:32 -0600

    Red Light |

--- dodoku/sandbox/__init__.py
+++ dodoku/sandbox/__init__.py
@@ -4,20 +4,32 @@ import dodoku.calculateHash as calculteHash
 grid = [3,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]
 print(calculteHash.calculateHash(grid))
 
-parms = {'op':'insert', 'cell':'r7c9', 'value':'3', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
+truth = ['ok', 'warning', 'ok', 'warning', 'warning', 'warning', 'warning', 'warning' ,'warning']
+truth1 = ['ok', 'warning', 'ok', 'ok', 'warning', 'warning', 'warning', 'ok', 'warning']
+truth2 = ['warning', 'ok', 'warning', 'warning', 'warning', 'warning', 'warning', 'ok', 'warning']
+truth3 = ['warning', 'warning', 'ok', 'warning', 'warning', 'warning', 'warning', 'ok', 'warning']
+truth4= ['warning', 'warning', 'warning', 'ok', 'ok', 'warning', 'ok', 'warning', 'warning']
+
+
+truth5 = ['warning', 'warning', 'ok', 'warning', 'ok', 'warning', 'ok', 'ok', 'warning']
+truth6 = ['warning', 'warning', 'warning', 'warning', 'ok', 'ok', 'warning', 'ok', 'warning']
+truth7= ['warning', 'ok', 'ok', 'warning', 'warning', 'ok', 'warning', 'warning', 'warning']
+truth8 = ['ok', 'warning', 'ok', 'warning', 'warning', 'ok', 'warning', 'ok', 'ok']
+parms = {'op':'insert', 'cell':'r5c7', 'value':'3', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
 actualResult = insert._insert(parms)  
 
 for i in range(1, 10):
-    parms = {'op':'insert', 'cell':'r9c8', 'value':str(i), 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
+    parms = {'op':'insert', 'cell':'r9c7', 'value':str(i), 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
     actualResult = insert._insert(parms)
-    print(str(i)+ " " +str(actualResult['status']))
+    if actualResult['status'] != truth8[i-1]:
+        print("false at: "+str(i))
     
 print(" ")
 
-for i in range(1, 16):
-    for j in range(1, 16):
-        cell = "r"+str(i)+"c"+str(j)
-        parms = {'op':'insert', 'cell':cell, 'value':'9', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
-        actualResult = insert._insert(parms)
-        print(cell + ": " + actualResult['status'])
+# for i in range(1, 16):
+#     for j in range(1, 16):
+#         cell = "r"+str(i)+"c"+str(j)
+#         parms = {'op':'insert', 'cell':cell, 'value':'9', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
+#         actualResult = insert._insert(parms)
+#         print(cell + ": " + actualResult['status'])
         
--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -1,5 +1,11 @@
 from unittest import TestCase
-import dodoku.status as recommend 
+import dodoku.recommend as recommend 
+import dodoku.calculateHash as calculteHash
 
 class RecommendTest(TestCase):
-        pass
+        def test_001_ShouldCreateKeys(self):
+            grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]
+            parms = {'op':'recommend', 'cell':'r7c9',  'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
+            actualResult = recommend._recommend(parms)
+            self.assertIn('recommendation', actualResult.keys())
+            self.assertIn('status', actualResult.keys())
