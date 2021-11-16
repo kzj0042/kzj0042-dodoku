9f35e36a7ba3db070ab8ad7858840138a6398f74
kzj0042 <kzj0042@auburn.edu>
2021-11-15 20:55:34 -0600

    Red Light |

--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -86,7 +86,18 @@ class RecommendTest(TestCase):
             actualStatus = {'status':actualResult['status']}
             actualRecommendation = {'recommendation':actualResult['recommendation']}
             self.assertDictEqual(expectedStatus, actualStatus)
-            self.assertDictEqual(expectedRecommendation, actualRecommendation)                                     
+            self.assertDictEqual(expectedRecommendation, actualRecommendation)    
+            
+        def test_009_ShouldReturnStatusListUpperCaseRowAndCol(self):
+            grid = '[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]'
+            parms = {'op':'recommend', 'cell':'R7C9', 'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
+            actualResult = recommend._recommend(parms)              
+            expectedStatus = {"status":"ok"}
+            expectedRecommendation = {"recommendation":[3,8]}
+            actualStatus = {'status':actualResult['status']}
+            actualRecommendation = {'recommendation':actualResult['recommendation']}
+            self.assertDictEqual(expectedStatus, actualStatus)
+            self.assertDictEqual(expectedRecommendation, actualRecommendation)                               
         
         #sad path
         def test_100_ShouldReturnErrorOutOfBoundsCell(self):
