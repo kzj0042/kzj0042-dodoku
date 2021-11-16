46e7928f496ebc2aca7a90e4ef03054694fed5b9
kzj0042 <kzj0042@auburn.edu>
2021-11-15 19:41:06 -0600

    Red Light |

--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -9,3 +9,14 @@ class RecommendTest(TestCase):
             actualResult = recommend._recommend(parms)
             self.assertIn('recommendation', actualResult.keys())
             self.assertIn('status', actualResult.keys())
+            
+        def test_002_ShouldReturnStatusListNominal(self):
+            grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]
+            parms = {'op':'recommend', 'cell':'r7c9',  'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
+            actualResult = recommend._recommend(parms)
+            expectedStatus = {"status":"ok"}
+            expectedRecommendation = {"recommend":[2,3,5,7,8]}
+            actualStatus = {'status':actualResult['status']}
+            actualRecommendation = {'recommendation':actualResult['recommendation']}
+            self.assertDictEqual(expectedStatus, actualStatus)
+            self.assertDictEqual(expectedRecommendation, actualRecommendation)
