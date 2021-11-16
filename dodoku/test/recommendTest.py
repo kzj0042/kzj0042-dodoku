488aaf2e3130f5050201478f7ddb1f085301d9ff
kzj0042 <kzj0042@auburn.edu>
2021-11-15 19:56:57 -0600

    Red Light |

--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -30,4 +30,15 @@ class RecommendTest(TestCase):
             actualStatus = {'status':actualResult['status']}
             actualRecommendation = {'recommendation':actualResult['recommendation']}
             self.assertDictEqual(expectedStatus, actualStatus)
-            self.assertDictEqual(expectedRecommendation, actualRecommendation)            
+            self.assertDictEqual(expectedRecommendation, actualRecommendation)      
+         
+        def test_004_ShouldReturnEmptyListImmutableHint(self):
+            grid = [0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]
+            parms = {'op':'recommend', 'cell':'r1c2',  'grid':grid, 'integrity':calculteHash.getEightCharactersOfHash(calculteHash.calculateHash(grid))}
+            actualResult = recommend._recommend(parms)
+            expectedStatus = {"status":"ok"}
+            expectedRecommendation = {"recommendation":[]}
+            actualStatus = {'status':actualResult['status']}
+            actualRecommendation = {'recommendation':actualResult['recommendation']}
+            self.assertDictEqual(expectedStatus, actualStatus)
+            self.assertDictEqual(expectedRecommendation, actualRecommendation)      
