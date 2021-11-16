6f1325ec4bfbc55ef04e6fb7ccc9657062cdca10
kzj0042 <kzj0042@auburn.edu>
2021-11-15 20:03:26 -0600

    Green Light |

--- dodoku/recommend.py
+++ dodoku/recommend.py
@@ -2,6 +2,19 @@ import dodoku.insert as insert
 import dodoku.calculateHash as calculateHash
  
 def _recommend(parms):
+    rowColNum = parms['cell'].lower()
+    rowColSplit = rowColNum.split('c')
+    try:
+        rowNum = int(rowColSplit[0][1:])
+        colNum = int(rowColSplit[1])
+    except ValueError:
+        result = {'status':'error: invalid cell'}
+        return result
+    
+    if rowNum>15 or colNum>15 or rowNum < 1 or colNum<1 or (rowNum<7 and colNum>9) or (rowNum>9 and colNum<7):
+        result = {'status':'error: invalid cell'}
+        return result
+    
     recommend = []
     status = "ok"
     grid = parms['grid']
